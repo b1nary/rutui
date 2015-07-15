96bff3cdedf830abeccb23bb845d46fbf9469e55
Joachim de Groot <jdegroot@web.de>
2015-07-15 22:09:24 +0200

    Use \e for escape char

--- lib/rutui/ansi.rb
+++ lib/rutui/ansi.rb
@@ -8,42 +8,39 @@ module RuTui
 	# shell/simulator on windows, and you'll be fine)
 	#
 	class Ansi
-		@@escape = "\x1b"
-		@@escape = "\033" if RUBY_PLATFORM =~ /(win32|w32)/
-
 		# Calculate color from RGB values
 		def self.rgb(red, green, blue);	16 + (red * 36) + (green * 6) + blue; end
 		# Set background color
-		def self.bg color; "#{@@escape}[48;5;#{color}m"; end
+		def self.bg color; "\e[48;5;#{color}m"; end
 		# Set text color
-		def self.fg color; "#{@@escape}[38;5;#{color}m"; end
+		def self.fg color; "\e[38;5;#{color}m"; end
 		# "Shortcut" to background color
 		def self.background color; self.bg color; end
 		# "Shortcut" to foreground/text color
 		def self.foreground color; self.fg color; end
 		# Set bold
-		def self.bold text; "#{@@escape}[1m#{text}"; end
+		def self.bold text; "\e[1m#{text}"; end
 		# Set thin
-		def self.thin text; "#{@@escape}[2m#{text}"; end
+		def self.thin text; "\e[2m#{text}"; end
 		# Set italic
-		def self.italic text; "#{@@escape}[3m#{text}"; end
+		def self.italic text; "\e[3m#{text}"; end
 		# Set underline
-		def self.underline text; "#{@@escape}[4m#{text}"; end
+		def self.underline text; "\e[4m#{text}"; end
 		# Set blink
-		def self.blink text; "#{@@escape}[5m#{text}"; end
+		def self.blink text; "\e[5m#{text}"; end
 		# Clear all color
-		def self.clear_color; "#{@@escape}[0m"; end
+		def self.clear_color; "\e[0m"; end
 		# Clear Screen/Terminal
-		def self.clear; "#{@@escape}[2J"; end
+		def self.clear; "\e[2J"; end
 		# set start
-		def self.set_start; "#{@@escape}[s"; end
+		def self.set_start; "\e[s"; end
 		# goto start
-		def self.goto_start; "#{@@escape}[u"; end
+		def self.goto_start; "\e[u"; end
 		# Go home
-		def self.go_home; "#{@@escape}[H"; end
+		def self.go_home; "\e[H"; end
 		# Goto position
-		def self.position x,y; "#{@@escape}[#{y};#{x}f"; end
+		def self.position x,y; "\e[#{y};#{x}f"; end
 		# Hide cursor
-		def self.hide_cursor; "#{@@escape}?25l"; end
+		def self.hide_cursor; "\e?25l"; end
 	end
 end
